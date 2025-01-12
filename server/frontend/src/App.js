bd3c1dae729b06ebe31eb80030342e76d7aa0d9c
Shubh9799 <yerunkar99.sy@gmail.com>
2025-01-12 02:47:44 -0500

    Added the Registration Part

--- server/frontend/src/App.js
+++ server/frontend/src/App.js
@@ -1,11 +1,14 @@
-import LoginPanel from "./components/Login/Login"
+import LoginPanel from "./components/Login/Login";
 import { Routes, Route } from "react-router-dom";
+import Register from "./components/Register/Register"; // Import Register component
 
 function App() {
   return (
     <Routes>
       <Route path="/login" element={<LoginPanel />} />
+      <Route path="/register" element={<Register />} /> {/* Add the Register route */}
     </Routes>
   );
 }
+
 export default App;
